332a57d6d6838a0ae708ee4f95a87064033d7cb3
lindroos <emptysmile1013@gmail.com>
2017-12-28 12:45:22 +0700

    update

--- 448. Find All Numbers Disappeared in an Array/main.go
+++ 448. Find All Numbers Disappeared in an Array/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"reflect"
 )
+
+// Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+// Find all the elements of [1, n] inclusive that do not appear in this array.
+
+// Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+// Example:
+
+// Input:
+// [4,3,2,7,8,2,3,1]
+
+// Output:
+// [5,6]
 
 func abs(n int) int {
 	if n < 0 {
@@ -18,6 +32,7 @@ func findDisappearedNumbers(nums []int) []int {
 			nums[abs(n)-1] = -nums[abs(n)-1]
 		}
 	}
+
 	ret := make([]int, 0, len(nums))
 	for i, n := range nums {
 		if n > 0 {
@@ -28,11 +43,22 @@ func findDisappearedNumbers(nums []int) []int {
 }
 
 func main() {
-	expect := []int{5, 6}
-	actual := findDisappearedNumbers([]int{4, 3, 2, 7, 8, 2, 3, 1})
-	if !reflect.DeepEqual(expect, actual) {
-		fmt.Println("expect and actual are not the same")
-		fmt.Printf("expect:%v\n", expect)
-		fmt.Printf("actual:%v\n", actual)
+	testCases := []struct {
+		description string
+		input       []int
+		expect      []int
+	}{
+		{
+			description: "testing 1",
+			input:       []int{4, 3, 2, 7, 8, 2, 3, 1},
+			expect:      []int{5, 6},
+		},
+	}
+
+	for _, tt := range testCases {
+		actual := findDisappearedNumbers(tt.input)
+		if !reflect.DeepEqual(tt.expect, actual) {
+			log.Fatalf("%s: expect:%v != actual:%v", tt.description, tt.expect, actual)
+		}
 	}
 }

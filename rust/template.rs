7bcbf08c6fd8eef11417155ff9e50297d3a5c334
tommady <tommady@users.noreply.github.com>
2020-01-05 17:55:50 +0800

    create bash file not done

---
+++ rust/template.rs
@@ -0,0 +1,20 @@
+/**
+ * __PROBLEM_DESC__
+ */
+
+#![allow(dead_code)]
+pub struct Solution {}
+
+__PROBLEM_DEFAULT_CODE__
+
+// Sample Test Case
+__PROBLEM_SAMPLE_TEST_CASE__
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test___PROBLEM_ID___solution() {
+    }
+}

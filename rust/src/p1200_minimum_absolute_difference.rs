16e4a37b6bdc4c4c3fd47ccd953601a80d890faa
tommady <tommady@users.noreply.github.com>
2023-07-05 12:18:11 +0000

    1200 ac

--- rust/src/p1200_minimum_absolute_difference.rs
+++ rust/src/p1200_minimum_absolute_difference.rs
@@ -1,4 +1,4 @@
-// Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements. Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows a, b are from arr a b b - a equals to the minimum absolute difference of any two elements in arr Example 1: Input: arr = [4,2,1,3] Output: [[1,2],[2,3],[3,4]] Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order. Example 2: Input: arr = [1,3,6,10,15] Output: [[1,3]] Example 3: Input: arr = [3,8,-10,23,19,-4,-14,27] Output: [[-14,-10],[19,23],[23,27]] Constraints: 2 = arr.length = 10^5 -10^6 = arr[i] = 10^6
+// Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements. Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows a, b are from arr a b b - a equals to the minimum absolute difference of any two elements in arr Example 1: Input: arr = [4,2,1,3] Output: [[1,2],[2,3],[3,4]] Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order. Example 2: Input: arr = [1,3,6,10,15] Output: [[1,3]] Example 3: Input: arr = [3,8,-10,23,19,-4,-14,27] Output: [[-14,-10],[19,23],[23,27]] Constraints: 2 = arr.length = 105 -106 = arr[i] = 106
 //
 //
 // [4,2,1,3]
@@ -10,20 +10,22 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn minimum_abs_difference(mut arr: Vec<i32>) -> Vec<Vec<i32>> {
-        arr.sort();
-        let mut the_min_diff: i32 = i32::MAX;
-        let mut ret = Vec::new();
+    pub fn minimum_abs_difference(arr: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut arr = arr;
+        arr.sort_unstable();
 
-        for i in 1..arr.len() {
-            let diff = (arr[i] - arr[i - 1]).abs();
+        let mut smallest = i32::MAX;
+        let mut ret = vec![];
 
-            if diff < the_min_diff {
-                the_min_diff = diff;
+        for pair in arr.windows(2) {
+            let diff = pair[1] - pair[0];
+
+            if diff < smallest {
+                smallest = pair[1] - pair[0];
                 ret.clear();
-                ret.push(vec![arr[i - 1], arr[i]]);
-            } else if diff == the_min_diff {
-                ret.push(vec![arr[i - 1], arr[i]]);
+                ret.push(vec![pair[0], pair[1]]);
+            } else if diff == smallest {
+                ret.push(vec![pair[0], pair[1]]);
             }
         }
 
@@ -39,19 +41,15 @@ mod tests {
     fn test_1200_solution() {
         assert_eq!(
             vec![vec![1, 2], vec![2, 3], vec![3, 4]],
-            Solution::minimum_abs_difference(vec![4, 2, 1, 3]),
+            Solution::minimum_abs_difference(vec![4, 2, 1, 3])
         );
         assert_eq!(
             vec![vec![1, 3]],
-            Solution::minimum_abs_difference(vec![1, 3, 6, 10, 15]),
+            Solution::minimum_abs_difference(vec![1, 3, 6, 10, 15])
         );
         assert_eq!(
             vec![vec![-14, -10], vec![19, 23], vec![23, 27]],
-            Solution::minimum_abs_difference(vec![3, 8, -10, 23, 19, -4, -14, 27]),
+            Solution::minimum_abs_difference(vec![3, 8, -10, 23, 19, -4, -14, 27])
         );
-        assert_eq!(
-            vec![vec![-25, -23]],
-            Solution::minimum_abs_difference(vec![-25, -51, -29, -23, 57, -18]),
-        )
     }
 }
